69da3677eb48373bcf34d0274b6c2b58cea9c5c6
Apoorva Raj <99551949+ApoorvaRaj123@users.noreply.github.com>
2026-05-28 23:22:19 +0530

    Create index2.ts
    Added interfaces and arrays in TS

---
+++ Typescript/index2.ts
@@ -0,0 +1,86 @@
+
+function isEven(num:number): boolean {
+    if(num%2==0){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
+// Interface and types for complex codebases and arguments
+
+interface User {
+    name: string;
+    age: number;
+    address: {
+        city: string;
+        country: string;
+        pincode: number;
+    }
+}
+
+
+let user: User = {
+    name:"john Doe",
+    age: 25,
+    address:{
+        city: "NYC",
+        country: "india",
+        pincode: 15006
+    }
+}
+
+
+function isLegal(user:User): boolean {
+    return user.age >= 18;
+}
+
+console.log(isLegal(user));
+
+
+// return the legal users in an array.
+
+interface UserType {
+    firstName: string,
+    lastName: string,
+    age: number
+}
+
+
+let user1: UserType = {
+    firstName:"Joah",
+    lastName: "Doe",
+    age:18
+}
+let user2: UserType = {
+    firstName:"Joahn",
+    lastName: "Dooe",
+    age:21
+}
+let user3: UserType = {
+    firstName:"Joah2",
+    lastName: "Doe2",
+    age:11
+}
+let user4: UserType = {
+    firstName:"Joah4",
+    lastName: "Doe4",
+    age:17
+}
+let user5: UserType = {
+    firstName:"Daniel",
+    lastName: "Park",
+    age:20
+}
+let user6: UserType = {
+    firstName:"Johan",
+    lastName: "Seong",
+    age:23
+}
+
+let arr = [user1,user2,user3,user4,user5,user6];
+
+const result = arr.filter((x) => x.age > 18);
+
+console.log(result);
